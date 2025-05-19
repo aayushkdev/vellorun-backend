494d1314c21d4d66799d599e56ec6c2ad92d0720
Aayush Kumar <code@aayushk.dev>
2025-05-20 00:32:45 +0530

    Add badges and fix unique username issue

--- api/utils.py
+++ api/utils.py
@@ -16,3 +16,28 @@ class IsSuperUserOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True 
         return request.user and request.user.is_authenticated and request.user.is_superuser
+
+
+def update_user_badges(user):
+    visited_places = Place.objects.filter(visit__user=user)
+    place_description = {place.description.lower() for place in visited_places}
+    categories = {place.category.lower() for place in visited_places if place.category}
+    badges = []
+
+    if "library" in place_description or any("academic" in name for name in place_description):
+        badges.append("nerd")
+
+    if "food" in categories:
+        badges.append("foodie")
+
+    if "rnr" in place_description or "arcade" in place_description:
+        badges.append("gamer")
+
+    if "fitness" in categories:
+        badges.append("jock")
+
+    if any("hostel" in name for name in place_description) and categories == {"campus"}:
+        badges.append("sleepyhead")
+
+    user.badges = list(set(badges))
+    user.save()
